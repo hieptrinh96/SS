d0058c25c38c1a427b0efe049c4fef23cda6f009
Hiep Trinh <xmikecolx@gmail.com>
2022-06-22 11:41:58 -0500

    adding elements to html page

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -4,6 +4,18 @@
 // const p = document.querySelector('p')
 // p.remove();
 
+const notes = [{
+    title: 'my next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
+
 // Query all and remove
 const ps = document.querySelectorAll('p');
 ps.forEach(p => {
--- todo-app/notes-app.js
+++
@@ -1,4 +0,0 @@
-const p = document.querySelectorAll('p');
-p.forEach(ps => {
-    if (ps.textContent.includes('the')) ps.remove();
-})
