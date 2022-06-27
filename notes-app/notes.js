7dd147277046ed69a3ee12109952a1ed63dd9b39
Hiep Trinh <xmikecolx@gmail.com>
2022-06-27 11:07:56 -0500

    changes

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,92 +1,24 @@
-// DOM - docuument object model
-// document is an object
-// Query and remove takes the first match it finds
-// const p = document.querySelector('p')
-// p.remove();
+const notes = getSavedNotes();
 
-const notes = [{
-    title: 'my next trip',
-    body: 'I would like to go to Spain'
-}, {
-    title: 'Habbits to work on',
-    body: 'Exercise. Eating a bit better.'
-}, {
-    title: 'Office modification',
-    body: 'Get a new seat'
-}]
-
-//to render our information 
-// create a new object with an empty string
 const filters = {
     searchText: ''
 }
-// create a new function that filters what we want 
-const renderNotes = (notes, filters) => {
-    const filteredNotes = notes.filter(note => {
-        // in this case, we want to filter the notes obj so it only contains what we put
-        // which is why we put filters, our empty string, and the searchText
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
-    // this line would clear what we've been adding 
-    document.querySelector('#notes').innerHTML = '';
-    filteredNotes.forEach(note => {
-        const noteEl = document.createElement('p');
-        // this iterates and creates new p elements that is the note.title
-        noteEl.textContent = note.title;
-        // we're just adding them to our div id
-        document.querySelector('#notes').appendChild(noteEl);
-    })
-}
 renderNotes(notes, filters);
 
-// this gives us access to our button element. 
-// the .addEventListener enables us to choose what happens to the button 
-// since we chose click, when we click on the button, the arrow function runs 
-document.querySelector('#create-note').addEventListener('click', e => {  
-    e.target.textContent = 'The button was pressed'
+document.querySelector('#create-note').addEventListener('click', (e) => {
+    notes.push({
+        title: '',
+        body: ''
+    })
+    savedNotes(notes);
+    renderNotes(notes, filters);
 })
- 
 
-// since querySelectorAll is to get all the elements, using the index 1 gives us access to the second button 
-// we can also set unique id's which is easier to dete rmine which button we're working on
-
-// we changed the querySelectorAll for just the selector since we have the unique id's
-// sidenote, if we didn't use the unique id's the order of the buttons would give us errors if they 
-// weren't in the order that we wrote it. 
-// with the unique id's we can change the order however we want without issue
-
-// id's use # while classes use .
-// selector is used for individual id's, selectorAll is used for classes
-// document.querySelector('#remove-all').addEventListener('click', () => {
-//     document.querySelectorAll('.note').forEach(note => note.remove())
-// })
-
-document.querySelector('#search-text').addEventListener('input', (e) => {
-    filters.searchText = e.target.value;
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
     renderNotes(notes, filters)
 })
-document.querySelector('#for-fun').addEventListener('change', (e) => {
-    console.log(e.target.checked)
+
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+    console.log(e.target.value)
 })
-// document.querySelector('#name-form').addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     // e.target gives us access to DOM and the .elements gives us access to the values of form
-//     console.log(e.target.elements.firstName.value)
-//     e.target.elements.firstName.value = '';
-// })
-
-// you can also chain these together (refer to udemy for reference)
-
-// Query all and remove
-// const ps = document.querySelectorAll('p');
-// ps.forEach(p => {
-//     p.textContent = '***********'
-    // console.log(p.textContent)
-    // p.remove();
-// })
-
-
-// set up a div so we can move it anywehre we want 
-// we can target specific one with the id's 
-// the innerHTML is used to clear the info 
- 
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,100 +1,29 @@
-const todos = [{
-    text: 'Order cat food',
-    completed: false
-}, {
-    text: 'Clean kitchen',
-    completed: true
-}, {
-    text: 'Buy food',
-    completed: true
-}, {
-    text: 'Do work',
-    completed: false
-}, {
-    text: 'Exercise',
-    completed: true
-}]
+let todos = getSavedTodos()
 
-
-// print summary message with p 
-                    // start   
 const filters = {
     searchText: '',
     hideCompleted: false
 }
-const renderTodos = (todos, filters) => {
-    const filteredTodos = todos.filter(todo => {
-        const searchTextToMatch =  todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
-        const hideCompletedMatch = !filters.hideCompleted || !todo.completed
-        return searchTextToMatch && hideCompletedMatch
-    })
-    const incomplete = filteredTodos.filter(todo => {
-        return !todo.completed
-    })
 
-    document.querySelector('#chores').innerHTML = '';
-    // create a new h2 element
-    const printMessage = document.createElement('h2');
-    // have it dynamically print out the number of tasks that are incomplete
-    printMessage.textContent = `You have ${incomplete.length} todos left`;
-    // push it into the body element
-    document.querySelector('#chores').appendChild(printMessage)
-    
-    // iterate through array 
-    filteredTodos.forEach(todo => {
-        // create a new element for each item in the array 
-        const p = document.createElement('p');
-        // set the textContent to whatever the todo.text is 
-        p.textContent = todo.text;
-        // finally push it into the body 
-        document.querySelector('#chores').appendChild(p);
-    })
-}
 renderTodos(todos, filters)
-// create a new function                   
-// creating a new p element
-// const todoList = document.createElement('p');
-// iterate through our array to find the incompleted tasks
-                        // end
-// creqate access to the button element
-// changing the button to use it's unique id
-// document.querySelector('#start').addEventListener('click', (e) => {
-//     console.log('New task created') ;
-// })
-//print paragraph for each to do, use text value
 
-// document.querySelector('#Enter-Tasks').addEventListener('input', (e) => {
-//     console.log(e.target.value);
-// })
-
-document.querySelector('#search-text').addEventListener('input', (e) => {
+document.querySelector('#search-text').addEventListener('input', function (e) {
     filters.searchText = e.target.value
     renderTodos(todos, filters)
 })
 
-document.querySelector('#Hide-Completed').addEventListener('change', (e) => {
-      filters.hideCompleted = e.target.checked;
-      renderTodos(todos, filters);
+document.querySelector('#new-todo').addEventListener('submit', function (e) {
+    e.preventDefault()
+    todos.push({
+        text: e.target.elements.text.value,
+        completed: false
+    })
+    saveTodos(todos)
+    renderTodos(todos, filters)
+    e.target.elements.text.value = ''
 })
 
-// document.querySelector('#new-todo').addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     todos.push({
-//         text: e.target.elements.text.value,
-//         completed: false
-//     })
-//     renderTodos(todos, filters);
-//     e.target.elements.text.value = '';
-// })
-
-
-// this hides all the text and turns them into whatever we put in the textContent
-// p.forEach(ps => {
-    //     ps.textContent = '**********'
-    // })
-    //  adding a new element
-    //  const newParagraph = document.createElement('p')
-    //  newParagraph.textContent = 'This is a new element from Javascript'
-    //  // this adds a new paragraph to the body. works just like array.push 
-//  document.querySelector('body').appendChild(newParagraph);
-
+document.querySelector('#hide-completed').addEventListener('change', function (e) {
+    filters.hideCompleted = e.target.checked
+    renderTodos(todos, filters)
+})
