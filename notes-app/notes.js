a364b7c36fc20c79f14d143bbb86eaea30c1c8cd
Hiep Trinh <xmikecolx@gmail.com>
2022-06-28 14:27:50 -0500

    continued progress

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,24 +1,79 @@
-const notes = getSavedNotes();
+// DOM stands for document Object Model 
+const notes = [{
+    title: 'my next trip',
+    body: 'I would like to go to Spain'
+    }, {
+        title: 'Habbits to work on',
+        body: 'Exercise. Eat a bit better.'
+    }, {
+        title: 'office modifications',
+        body: 'Get a new seat'
+    }]
+// we can mutate that specific p tag with querySelector
+// querySelector only looks at the first p element, will not affect the other p's
+// const p = document.querySelector('p')
+// // // removes the element selected
+// // p.remove();
 
+
+// querySelectorAll returns an array
+
+// // we can use a forEach method to remove every p element;
+// // ps.forEach(p => p.remove());
+
+// // QuerySelectorAll affects all the p elements 
+// const ps = document.querySelectorAll('p')
+
+// // this will change the value of each p element to whatever we put in the string 
+// ps.forEach(p => console.log(p.textContent = '******'));
+// // how to 'render' which is just adding elements
+// // create a new element, update the textContent, and pick a place to put it
+// const newParagraph = document.createElement('p');
+// newParagraph.textContent = 'This is a new element from JS';
+// // first we select where we want to add our new p element.
+// // then we use appendChild to add that element to the end of the body element
+// // anytime you use appendChild, it will add the content to the end of that element
+// // so since we want to add it to the body, it will be added after all the other elements in the body 
+// document.querySelector('body').appendChild(newParagraph)
 const filters = {
     searchText: ''
 }
-renderNotes(notes, filters);
-
-document.querySelector('#create-note').addEventListener('click', (e) => {
-    notes.push({
-        title: '',
-        body: ''
+// our function allows us to filter/find notes
+const renderNotes = (notes, filters) => {
+    // we're returning the notes.title that matches wat the user inputs on the webpage
+    const filteredNotes = notes.filter(note => note.title.toLowerCase().includes(filters.searchText.toLowerCase()));
+    // This allows us to remove the notes that do not match the one the user is searching for
+    // we want to use a div since if we use body, we would clear everything 
+    document.querySelector('#notes').innerHTML = '';
+    // this creates a new p tag for the ones the user searches for
+    filteredNotes.forEach(note => {
+        const noteEl = document.createElement('p');
+        noteEl.textContent = note.title;
+        // we're adding the div element since 
+        document.querySelector('#notes').appendChild(noteEl);
     })
-    savedNotes(notes);
+}
+
+//  call the function once 
+renderNotes(notes, filters)
+
+// to get access to an element
+document.querySelector('#create-note').addEventListener('click', (e) => {
+    // the e.target allows us to change the button once it is clicked 
+    e.target.textContent = 'The button was clicked';
+})
+
+
+
+// to target everything in a class, we have to use querySelectorAll and add a period before
+// the class name
+// document.querySelectorAll('.notes').forEach(note => note.remove())
+
+document.querySelector('#search-text').addEventListener('input', (e) => {
+    filters.searchText = e.target.value
     renderNotes(notes, filters);
 })
 
-document.querySelector('#search-text').addEventListener('input', function (e) {
-    filters.searchText = e.target.value
-    renderNotes(notes, filters)
-})
-
-document.querySelector('#filter-by').addEventListener('change', function (e) {
+document.querySelector('#filtered-by').addEventListener('change', (e) => {
     console.log(e.target.value)
 })
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,29 +1,93 @@
-let todos = getSavedTodos()
+const todos = [{
+    text: 'Order dog food',
+    completed: false
+    }, {
+        text: 'Clean kitchen',
+        completed: true
+    }, {
+        text: 'buy food',
+        completed: true
+    }, {
+        text: 'Study more',
+        completed: false
+    }, {
+        text: 'Exercise',
+        completed: true
+    }];
 
-const filters = {
-    searchText: '',
-    hideCompleted: false
-}
+    const filters = {
+        searchText: '',
+        hideCompleted: false 
+    }
 
-renderTodos(todos, filters)
+    const renderedTodos = (todos, filters) => {
+        // we still need to be able to search through the todos 
+        const filteredTodos = todos.filter(task => {
+            const searchTextMatch = task.text.toLowerCase().includes(filters.searchText.toLowerCase());
+            const hideCompletedMatch = !filters.hideCompleted || !task.completed;
+            return searchTextMatch && hideCompletedMatch;
+        })
+        // filter out which tasks are left, save the value to a variable
+        const incompleteTasks = filteredTodos.filter(task => !task.completed)
+        // clearing the inputs
+        document.querySelector('#todo').innerHTML = '';
+        // create a new h2 tag
+        const summary = document.createElement('h2');
+        // set the textContent of summary to have our message
+        summary.textContent = `You have ${incompleteTasks.length} tasks left`;
+        // add that summary variable to our body
+        // before we create new p tags, we want to remove the other tasks that don't match the one we want
+        
+        document.querySelector('#todo').appendChild(summary)
+        document.querySelector('#search-text').innerHTML = '';
+        filteredTodos.forEach(task => {
+            // iterate through our todos array and create a new tag for each item
+            const paragraphs = document.createElement('p');
+            // set the content of each p tag to our todo.text
+            paragraphs.textContent = task.text;
+            // add each todo.text to our body
+            document.querySelector('#todo').appendChild(paragraphs);
+        })
+    }
+    renderedTodos(todos, filters);
 
-document.querySelector('#search-text').addEventListener('input', function (e) {
+// since we created the button tag in the html, we don't need to create it here
+// we do need to use the eventListener to interact with the button though
+
+
+
+// remove all the p tags with the word 'the';
+// set a variable to access all the p tags with querySelectorAll
+// then iterate through with filter
+//  const ps = document.querySelectorAll('p')
+// ps.forEach(p => {
+    //     // we have to use textContent since we're searching through the value of the text
+    //     if (p.textContent.includes('the')) p.remove();
+    // })
+
+document.querySelector('#search-text').addEventListener('input', (e) => {
     filters.searchText = e.target.value
-    renderTodos(todos, filters)
+    renderedTodos(todos, filters)
 })
-
-document.querySelector('#new-todo').addEventListener('submit', function (e) {
-    e.preventDefault()
+document.querySelector('#task-adder').addEventListener('submit', (e) => {
+    e.preventDefault();
+    // we're adding a new task to our todo list 
     todos.push({
+        // we're setting the value of whatever that task is to what the user inputs
         text: e.target.elements.text.value,
         completed: false
     })
-    saveTodos(todos)
-    renderTodos(todos, filters)
-    e.target.elements.text.value = ''
+    renderedTodos(todos, filters);
+    e.target.elements.text.value = '';
 })
 
-document.querySelector('#hide-completed').addEventListener('change', function (e) {
-    filters.hideCompleted = e.target.checked
-    renderTodos(todos, filters)
+document.querySelector('#hide-completed').addEventListener('change', (e) => {
+    // this will update our hideCompleted from false to true 
+    filters.hideCompleted = e.target.checked;
+    renderedTodos(todos, filters);
 })
+
+// create a new checkbox and setup even listener (hide completed)
+// create new hidecompleted filter (default false)
+// update hidecompleted and rerender list on checkbox change
+// setup rendertodos to remove completed items
