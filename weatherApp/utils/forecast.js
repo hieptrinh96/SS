4db39dc628bf411dd9688146fff3a137d51fb759
Hiep Trinh <xmikecolx@gmail.com>
2022-07-30 17:38:53 -0500

    building JSON http endpoint

--- weatherApp/utils/forecast.js
+++ weatherApp/utils/forecast.js
@@ -3,10 +3,10 @@ const request = require('request');
 const forecast = (latitude, longitude, cb) => {
     const url = 'http://api.weatherstack.com/current?access_key=9416c59f17dc33e87d34406ab9c48d63&query=' + latitude + ',' + longitude + '&units=f';
 
-    request({ url: url, json: true}, (error, response) => {
+    request({ url: url, json: true}, (error, { body }) => {
         if (error) cb('Unable to connect to weather service!', undefined);
         else if (response.body.error) cb('Unable to find location', undefined)
-        else cb(undefined, response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out.')
+        else cb(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. There is a ' + body.current.precip + '% chance of rain.');
     })
 }
 
